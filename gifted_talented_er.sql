7f19921897a0e1f45c67cc92c58e50f8e153f096
Joe Zuvanich <zuvanjo@tulsaschools.org>
2023-12-18 16:42:56 -0600

    Added test names to where clause.

--- gifted_talented_er.sql
+++ gifted_talented_er.sql
@@ -1,78 +1,71 @@
 SELECT
-  S.ID,
-  S.STUDENT_NUMBER,
-  S.LASTFIRST,
-  S.DCID,
-  SX.GIFTED,
+  DISTINCT STUDENTTEST.TEST_DATE,
+  STUDENTS.ID,
+  STUDENTS.STUDENT_NUMBER,
+  STUDENTS.LASTFIRST,
+  STUDENTS.DCID,
+  S_OK_STU_X.GIFTED,
   CASE
-    WHEN REGEXP_REPLACE(TS.NAME, CHR(94)
-                                 || 'a-zA-Z0-9'
-                                 || CHR(93)) = 'Composite(VQ) S Age Score' AND STS.NUMSCORE > 1 THEN
+    WHEN REGEXP_REPLACE(TESTSCORE.NAME, CHR(94)
+                                        || 'a-zA-Z0-9'
+                                        || CHR(93)) = 'Composite(VQ) S Age Score' AND STUDENTTESTSCORE.NUMSCORE > 119 THEN
       'Certified by CogAT Abilities'
-    WHEN REGEXP_REPLACE(TS.NAME, CHR(94)
-                                 || 'a-zA-Z0-9'
-                                 || CHR(93)) = 'Composite(VN) S Age Score' AND STS.NUMSCORE > 1 THEN
+    WHEN REGEXP_REPLACE(TESTSCORE.NAME, CHR(94)
+                                        || 'a-zA-Z0-9'
+                                        || CHR(93)) = 'Composite(VN) S Age Score' AND STUDENTTESTSCORE.NUMSCORE > 119 THEN
       'Certified  by CogAT Abilities'
-    WHEN REGEXP_REPLACE(TS.NAME, CHR(94)
-                                 || 'a-zA-Z0-9'
-                                 || CHR(93)) = 'Composite(QN) S Age Score' AND STS.NUMSCORE > 1 THEN
+    WHEN REGEXP_REPLACE(TESTSCORE.NAME, CHR(94)
+                                        || 'a-zA-Z0-9'
+                                        || CHR(93)) = 'Composite(QN) S Age Score' AND STUDENTTESTSCORE.NUMSCORE > 119 THEN
       'Certified by CogAT Abilities'
-    WHEN REGEXP_REPLACE(TS.NAME, CHR(94)
-                                 || 'a-zA-Z0-9'
-                                 || CHR(93)) = 'Composite(VQN) S Age Score' AND STS.NUMSCORE > 1 THEN
+    WHEN REGEXP_REPLACE(TESTSCORE.NAME, CHR(94)
+                                        || 'a-zA-Z0-9'
+                                        || CHR(93)) = 'Composite(VQN) S Age Score' AND STUDENTTESTSCORE.NUMSCORE > 119 THEN
       'Certified by  CogAT Abilities'
-    WHEN REGEXP_REPLACE(TS.NAME, CHR(94)
-                                 || 'a-zA-Z0-9'
-                                 || CHR(93)) = 'OTIS_SEM_ADJUST_ABILITY_INDEX' AND STS.NUMSCORE > 1 THEN
+    WHEN REGEXP_REPLACE(TESTSCORE.NAME, CHR(94)
+                                        || 'a-zA-Z0-9'
+                                        || CHR(93)) = 'OTIS_SEM_ADJUST_ABILITY_INDEX' AND STUDENTTESTSCORE.NUMSCORE > 119 THEN
       'Certified  OLSAT (Otis-Lennon)'
-    WHEN REGEXP_REPLACE(TS.NAME, CHR(94)
-                                 || 'a-zA-Z0-9'
-                                 || CHR(93)) = 'NAGLIERI_SEM_ADJUST_ABILITY_INDEX' AND STS.NUMSCORE > 1 THEN
+    WHEN REGEXP_REPLACE(TESTSCORE.NAME, CHR(94)
+                                        || 'a-zA-Z0-9'
+                                        || CHR(93)) = 'NAGLIERI_SEM_ADJUST_ABILITY_INDEX' AND STUDENTTESTSCORE.NUMSCORE > 119 THEN
       'Certified  by NNAT3 (Naglieri)'
-  END                                            CERTIFIED_BY,
-  ST.TEST_DATE,
-  ST.DCID                                        AS TEST_KEY,
-  ST.GRADE_LEVEL,
-  ST.ID                                          AS STUDENT_TEST_ID,
-  T.NAME                                         AS TEST_NAME,
-  TS.ID                                          AS SUBTEST_ID,
-  LOWER(REGEXP_REPLACE(TS.NAME, CHR(94)
-                                || 'a-zA-Z0-9'
-                                || CHR(93), '')) AS SUBTEST_KEY,
-  TS.NAME                                        AS SUBTEST_NAME,
-  STS.NUMSCORE,
-  STS.PERCENTSCORE,
-  STS.ALPHASCORE
+  END                                                   CERTIFIED_BY,
+  STUDENTTEST.DCID                                      AS TEST_KEY,
+  STUDENTTEST.GRADE_LEVEL,
+  STUDENTTEST.ID                                        AS STUDENT_TEST_ID,
+  TEST.NAME                                             AS TEST_NAME,
+  TESTSCORE.ID                                          AS SUBTEST_ID,
+  LOWER(REGEXP_REPLACE(TESTSCORE.NAME, CHR(94)
+                                       || 'a-zA-Z0-9'
+                                       || CHR(93), '')) AS SUBTEST_KEY,
+  TESTSCORE.NAME                                        AS SUBTEST_NAME,
+  STUDENTTESTSCORE.NUMSCORE,
+  STUDENTTESTSCORE.PERCENTSCORE,
+  STUDENTTESTSCORE.ALPHASCORE
 FROM
-  STUDENTS         S
-  INNER JOIN STUDENTTEST ST
-  ON S.ID = ST.STUDENTID
-  JOIN S_OK_STU_X SX
-  ON S.DCID = SX.STUDENTSDCID INNER JOIN TEST T
-  ON T.ID = ST.TESTID
-  INNER JOIN STUDENTTESTSCORE STS
-  ON ST.ID = STS.STUDENTTESTID
-  INNER JOIN TESTSCORE TS
-  ON TS.ID = STS.TESTSCOREID
+  STUDENTS
+  INNER JOIN STUDENTTEST
+  ON STUDENTS.ID = STUDENTTEST.STUDENTID
+  JOIN S_OK_STU_X
+  ON STUDENTS.DCID = S_OK_STU_X.STUDENTSDCID INNER JOIN TEST
+  ON TEST.ID = STUDENTTEST.TESTID
+  INNER JOIN STUDENTTESTSCORE
+  ON STUDENTS.ID = STUDENTTESTSCORE.STUDENTTESTID
+  INNER JOIN TESTSCORE
+  ON TESTSCORE.ID = STUDENTTESTSCORE.TESTSCOREID
 WHERE
-  ST.STUDENTID = 230671
-  AND (REGEXP_REPLACE(TS.NAME, CHR(94)
-                               || 'a-zA-Z0-9'
-                               || CHR(93)) = 'Composite(VQ) S Age Score'
-  OR REGEXP_REPLACE(TS.NAME, CHR(94)
-                             || 'a-zA-Z0-9'
-                             || CHR(93)) = 'Composite(VN) S Age Score'
-  OR REGEXP_REPLACE(TS.NAME, CHR(94)
-                             || 'a-zA-Z0-9'
-                             || CHR(93)) = 'Composite(QN) S Age Score'
-  OR REGEXP_REPLACE(TS.NAME, CHR(94)
-                             || 'a-zA-Z0-9'
-                             || CHR(93)) = 'Composite(VQN) S Age Score'
-  OR REGEXP_REPLACE(TS.NAME, CHR(94)
-                             || 'a-zA-Z0-9'
-                             || CHR(93)) = 'OTIS_SEM_ADJUST_ABILITY_INDEX'
-  OR REGEXP_REPLACE(TS.NAME, CHR(94)
-                             || 'a-zA-Z0-9'
-                             || CHR(93)) = 'NAGLIERI_SEM_ADJUST_ABILITY_INDEX')
+  UPPER(S_OK_STU_X.GIFTED) = 'YES'
+  AND (TEST.NAME = 'NAGL'
+  OR TEST.NAME = 'OTIS'
+  OR TEST.NAME = 'COGAT')
+  AND (TESTSCORE.NAME = 'NAGLIERI_SEM_ADJUST_ABILITY_INDEX'
+  OR TESTSCORE.NAME = 'OTIS_SEM_ADJUST_ABILITY_INDEX'
+  OR TESTSCORE.NAME = 'Composite(VQ) S Age Score'
+  OR TESTSCORE.NAME = 'Composite(VN) S Age Score'
+  OR TESTSCORE.NAME = 'Composite(QN) S Age Score'
+  OR TESTSCORE.NAME = 'Composite(VQN) S Age Score')
+  AND STUDENTS.SCHOOLID != '999999'
 ORDER BY
-  ST.TEST_DATE
+  STUDENTS.STUDENT_NUMBER,
+  STUDENTTEST.TEST_DATE
